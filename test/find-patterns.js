e54b97a2eee35a2aa2a8cceea579af6d522660e2
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-07 23:53:19 +0200

    test(find-patterns): throw if no arguments are given

--- test/find-patterns.js
+++ test/find-patterns.js
@@ -5,3 +5,8 @@ test('when calling find-patterns', t => {
 	const actual = findPatterns();
 	t.ok(typeof actual.then === 'function', 'it should return a promise');
 });
+
+test('when calling with no arguments', t => {
+	const actual = findPatterns();
+	t.throws(actual, TypeError, 'it should throw a TypeError');
+});
