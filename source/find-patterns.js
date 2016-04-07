01a6f45ecad73368020ba3c955209478c6940abc
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-08 00:11:14 +0200

    feat(find-patterns): throw RangeError if file/directory does not exist

--- source/find-patterns.js
+++ source/find-patterns.js
@@ -2,4 +2,5 @@ export default async (searchPath) => {
 	if (!searchPath) {
 		throw new TypeError('Missing first argument searchPath');
 	}
+	throw new RangeError(`File or directory '${searchPath} not found`);
 }
