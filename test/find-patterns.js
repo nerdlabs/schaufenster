668362e66de3de6e42fd4bfc97d1a4b92077e2c2
Mario Nebl <hello@herebecode.com>
2016-04-08 00:09:00 +0200

    test(find-patterns): define behaviour for non-existing paths

--- test/find-patterns.js
+++ test/find-patterns.js
@@ -5,3 +5,8 @@ test('when calling find-patterns with no arguments', t => {
 	const actual = findPatterns();
 	t.throws(actual, TypeError, 'it should throw a TypeError');
 });
+
+test('when calling find-patterns with a non-existing path', t => {
+	const actual = findPatterns('./something/very/unlikely');
+	t.throws(actual, RangeError, 'it should throw a RangeError');
+});
