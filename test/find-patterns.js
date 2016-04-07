8666aab62705508b45de31911b486ad0a02183d3
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-08 00:21:44 +0200

    test(find-patterns): return empty array if no patterns are found

--- test/find-patterns.js
+++ test/find-patterns.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import mockFs from 'mock-fs';
 import findPatterns from '../source/find-patterns';
 
 test('when calling find-patterns with no arguments', t => {
@@ -9,4 +10,14 @@ test('when calling find-patterns with no arguments', t => {
 test('when calling find-patterns with a non-existing path', t => {
 	const actual = findPatterns('./something/very/unlikely');
 	t.throws(actual, RangeError, 'it should throw a RangeError');
+});
+
+test('when no patterns are found in given directory', async t => {
+	mockFs({
+		'components': {
+			'unrelated.js': 'blah'
+		}
+	});
+	const actual = await findPatterns('./components');
+	t.deepEqual(actual, [], 'it should return an empty array');
 });
