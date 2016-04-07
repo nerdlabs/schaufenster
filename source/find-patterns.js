295d27c457217d0c5cd16382101b4d94438b06b2
Mario Nebl <hello@herebecode.com>
2016-04-08 00:31:56 +0200

    feat(find-patterns): return empty array if searchPath can be accessed

--- source/find-patterns.js
+++ source/find-patterns.js
@@ -1,6 +1,29 @@
-export default async (searchPath) => {
-	if (!searchPath) {
-		throw new TypeError('Missing first argument searchPath');
+import {stat} from 'fs';
+import denodeify from 'denodeify';
+
+const fsStat = denodeify(stat);
+
+async function tryStat(searchPath) {
+	try {
+		return await fsStat(searchPath);
+	} catch (error) {
+		return null;
 	}
-	throw new RangeError(`File or directory '${searchPath} not found`);
+}
+
+export default async (searchPath) => {
+	if (typeof searchPath !== 'string') {
+		throw new TypeError(
+			'First argument searchPath must be of type string, received',
+			typeof searchPath
+		);
+	}
+
+	const pathStat = await tryStat(searchPath);
+	
+	if (!pathStat) {
+		throw new RangeError(`File or directory '${searchPath} not found`);
+	} else {
+		return [];
+	}
 }
