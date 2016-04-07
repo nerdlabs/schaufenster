c1a05cd54a890cad16fd9dd41867630243803ced
Mario Nebl <hello@herebecode.com>
2016-04-08 00:05:05 +0200

    test(find-patterns): merge test cases

--- test/find-patterns.js
+++ test/find-patterns.js
@@ -1,12 +1,7 @@
 import test from 'ava';
 import findPatterns from '../source/find-patterns';
 
-test('when calling find-patterns', t => {
-	const actual = findPatterns();
-	t.ok(typeof actual.then === 'function', 'it should return a promise');
-});
-
-test('when calling with no arguments', t => {
+test('when calling find-patterns with no arguments', t => {
 	const actual = findPatterns();
 	t.throws(actual, TypeError, 'it should throw a TypeError');
 });
