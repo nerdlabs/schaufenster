d7a2e8e3e52c6525ecac3cdc206cc1d9174aeefe
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-09 23:45:14 +0200

    test(read-patterns): return empty array if input contains no patterns

--- test/read-pattern.js
+++ test/read-pattern.js
@@ -5,3 +5,8 @@ test('when calling with no arguments', t => {
 	const actual = readPatterns();
 	t.throws(actual, Error, 'it should throw an Error');
 });
+
+test('when calling with an empty array', async t => {
+	const actual = await readPatterns([]);
+	t.same(actual, [], 'it should return an empty array');
+});
