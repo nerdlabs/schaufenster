d07aff827df106b61955cc3be3b7924a3124c070
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-10 15:25:27 +0200

    test(parse-patterns): find JS dependencies

--- test/parse-patterns.js
+++ test/parse-patterns.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import unindent from 'unindent';
 import parsePatters from '../source/parse-patterns';
 
 test('when calling with no arguments', t => {
@@ -9,4 +10,19 @@ test('when calling with no arguments', t => {
 test('when calling with an empty array', async t => {
 	const actual = await parsePatters([]);
 	t.same(actual, [], 'it should return an empty array');
+});
+
+test('find dependencies', async t => {
+	const entry = unindent(`
+		import Text from '../text';
+		export default (props) => {
+			return (<button><Text /></button>);
+		};
+	`);
+	const actual = await parsePatters([{entry}]);
+	const expected = [{
+		entry,
+		dependencies: ['../text']
+	}];
+	t.same(actual, expected, 'it should parse entry file and find file dependencies');
 });
