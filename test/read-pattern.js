4e921c9115652fabe43ff15574c53982c1c95667
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-10 10:26:55 +0200

    test(read-patterns): read directory contents for each pattern

--- test/read-pattern.js
+++ test/read-pattern.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import mockFs from 'mock-fs';
 import readPatterns from '../source/read-patterns';
 
 test('when calling with no arguments', t => {
@@ -9,4 +10,21 @@ test('when calling with no arguments', t => {
 test('when calling with an empty array', async t => {
 	const actual = await readPatterns([]);
 	t.same(actual, [], 'it should return an empty array');
+});
+
+test(`when calling with valid pattern folders`, async t => {
+	mockFs({
+		'components/button': {
+			'package.json': '{"name":"button","version":"0.1.0","main":"button.js"}',
+			'button.js': 'blub'
+		}
+	});
+	const actual = await readPatterns([{path: './components/button'}]);
+	const expected = [{
+		path: './components/button',
+		files: ['button.js', 'package.json']
+	}];
+	console.log(actual, expected);
+	t.same(actual, expected, 'it should read the directory contents');
+	mockFs.restore();
 });
