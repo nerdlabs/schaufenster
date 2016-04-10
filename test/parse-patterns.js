4452bc1b26dbec235ff62542cb37719438d4ba59
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-10 14:45:05 +0200

    test(parse-patterns): no-op if input is empty array

--- test/parse-patterns.js
+++ test/parse-patterns.js
@@ -5,3 +5,8 @@ test('when calling with no arguments', t => {
 	const actual = parsePatters();
 	t.throws(actual, Error, 'it should throw an Error');
 });
+
+test('when calling with an empty array', async t => {
+	const actual = await parsePatters([]);
+	t.same(actual, [], 'it should return an empty array');
+});
