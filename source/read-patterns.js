4a52206eddbdc030c9495d9d4b379ae1bcb95904
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-10 11:11:10 +0200

    refactor(read-patterns): use invariant for argument checking

--- source/read-patterns.js
+++ source/read-patterns.js
@@ -1,12 +1,17 @@
 import {tryReaddir} from './lib/fs';
 import {zipWith} from 'lodash';
+import invariant from 'invariant';
 
 export default async (patterns) => {
-	if (typeof patterns === 'undefined') {
-		throw new Error('No arguments provided');
-	}
+	invariant(
+		Array.isArray(patterns),
+		'Argument must be of type Array. Instead got "%s"',
+		typeof patterns
+	);
+
 	const readPatternContents = patterns.map(({path}) => tryReaddir(path));
 	const patternFiles = await Promise.all(readPatternContents);
+
 	return zipWith(patterns, patternFiles, (pattern, files) => {
 		return {...pattern, files};
 	});
