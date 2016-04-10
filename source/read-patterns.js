6ffb67504dffc7807bd35d8b3bcc98f28ab5ed82
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-10 10:52:49 +0200

    feat(read-patterns): read directory contents for each pattern

--- source/read-patterns.js
+++ source/read-patterns.js
@@ -1,7 +1,24 @@
+import {readdir as readdirFs} from 'fs';
+import denodeify from 'denodeify';
+import {zipWith} from 'lodash';
+
+const readdir = denodeify(readdirFs);
+
+const tryReaddir = async path => {
+	try {
+		return await readdir(path);
+	} catch (error) {
+		return null;
+	}
+};
+
 export default async (patterns) => {
 	if (typeof patterns === 'undefined') {
 		throw new Error('No arguments provided');
-	} else {
-		return [];
 	}
+	const readPatternContents = patterns.map(({path}) => tryReaddir(path));
+	const patternFiles = await Promise.all(readPatternContents);
+	return zipWith(patterns, patternFiles, (pattern, files) => {
+		return {...pattern, files};
+	});
 };
