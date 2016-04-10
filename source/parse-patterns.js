d2010edaeef2f0c84451d49f35e96b9057ff5871
ZauberNerd <zaubernerd@zaubernerd.de>
2016-04-10 14:44:34 +0200

    feat(parse-patterns): validate arguments

---
+++ source/parse-patterns.js
@@ -0,0 +1,9 @@
+import invariant from 'invariant';
+
+export default async (patterns) => {
+	invariant(
+		Array.isArray(patterns),
+		'Argument must be of type Array. Instead got "%s"',
+		typeof patterns
+	);
+};
